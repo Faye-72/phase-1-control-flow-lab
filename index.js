40d94a5f69678d23683e6382ca224f5ea040df4e
faith mitei <faithmitey@gmail.com>
2022-12-05 14:03:31 +0300

    pending switch statement to ace it

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+let result
+if (distance <= 400 ){
+  result = 'This one is on me!'
+}else if(distance > 2000 && distance < 2500){
+result ='I will gladly take your thirty bucks.'
 }
+else if(distance >2500){
+  result = 'No can do.'
+}
+ return result
+}
+scuberGreetingForFeet(300)
+scuberGreetingForFeet(2001)
+scuberGreetingForFeet(2600)
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+let result
+result =  city ==="NYC" ? "Ok, sounds good." : "No go.";
+ return(result)
 }
+ternaryCheckCity("NYC")
+ternaryCheckCity("MOMBASA")
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  
+  switch(tip){
+    case 'the tip is generous':
+ console.log('Thank you so much.')
+    break;
+    case 'the tip is not as generous':
+    console.log('Thank you.')
+    break;
+    default:
+      console.log('Bye.')
+      break;
+  }
+ return(tip)
+}
+switchOnCharmFromTip("the tip is generous")
+switchOnCharmFromTip("'the tip is not as generous")
+switchOnCharmFromTip("is generousity")
